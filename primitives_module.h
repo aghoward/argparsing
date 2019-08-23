adfd9dcb63bcba58846d4488ffb740140b21a0ea
Austin Howard <austinghoward@gmail.com>
2019-08-23 13:31:25 -0400

    Include missing primitives

--- primitives_module.h
+++ primitives_module.h
@@ -18,9 +18,22 @@ namespace ap {
                     [] (std::string v) { return std::stoul(v, 0, 0); })
                     .build();
 
+                ctx.bind<short, std::string>(
+                    [] (std::string v) { return static_cast<short>(std::stoul(v, 0, 0)); })
+                    .build();
+
                 ctx.bind<int, std::string>(
                     [] (std::string v) { return std::stoi(v, 0, 0); })
                     .build();
+
+                ctx.bind<unsigned int, std::string>(
+                    [] (std::string v) { return static_cast<unsigned int>(std::stoul(v, 0, 0)); })
+                    .build();
+
+                ctx.bind<unsigned short, std::string>(
+                    [] (std::string v) { return static_cast<unsigned short>(std::stoul(v, 0, 0)); })
+                    .build();
+
                 ctx.bind<long, std::string>(
                     [] (std::string v) { return std::stol(v, 0, 0); })
                     .build();
